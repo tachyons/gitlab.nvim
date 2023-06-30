830ad873bd33f7d46c0bc374ed8e4806560ce737
Ash McKenzie <amckenzie@gitlab.com>
2023-06-30 20:04:23 +1000

    Levelled logging to /tmp/gitlab.vim.log

---
+++ lua/gitlab/version.lua
@@ -0,0 +1,14 @@
+local version = {}
+local version_string = "0.1.0"
+
+function version.version()
+  return version_string
+end
+
+function version.register(command)
+end
+
+function version.setup(options)
+end
+
+return version
---
+++ spec/gitlab/logging_spec.lua
@@ -0,0 +1,10 @@
+describe('gitlab.logging', function()
+  local version = require('gitlab.version')
+  local logging = require('gitlab.logging')
+
+  describe('format_line', function()
+    it('returns a line with timestamp, level, version and msg', function()
+      assert.are.equal("1234-56-78 12:34:56: INFO (" .. version.version() .. "): test", logging.format_line('test', 'INFO', "1234-56-78 12:34:56"))
+    end)
+  end)
+end)
