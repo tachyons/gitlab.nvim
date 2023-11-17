52167e7482f7edca767e5e01a76afcf45e9576dd
Erran Carey <me@errancarey.com>
2023-11-17 18:10:10 +0000

    Resolve "Enable integration code suggestions integration test when GITLAB_TOKEN contains required scopes"

--- spec/integration/categories/code_suggestions/telemetry_spec.lua
+++ spec/integration/categories/code_suggestions/telemetry_spec.lua
@@ -65,7 +65,7 @@ guard_describe('gitlab.commands.code_suggestions', conditions.snowplow_micro_ava
 
   guard_describe(
     'with valid personal access token',
-    conditions.token({ using = 'GITLAB_TOKEN', require_scopes = { 'api', 'read_user', 'nope' } }),
+    conditions.token({ using = 'GITLAB_TOKEN', require_scopes = { 'api', 'read_user' } }),
     function()
       before_each(function()
         local status, err = pcall(function()
