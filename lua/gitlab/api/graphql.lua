a48fb3cdd0fb258b4416339a8de6742f8a8a2acd
Erran Carey <ecarey@gitlab.com>
2024-03-06 01:03:50 +0000

    feat: Add duo project status check
    Prevent code suggestions usage on projects with Duo disabled. Add a healthcheck to reflect this status.

--- lua/gitlab/api/graphql.lua
+++ lua/gitlab/api/graphql.lua
@@ -14,11 +14,13 @@ local function graphql_url() --{{{
   }, '/')
 end --}}}
 
-function M.query(query)
+function M.execute(query, variables)
   if glab.available() then
     return glab.api(graphql_url(), {
+      method = 'POST',
       params = {
         query = query,
+        variables = variables and vim.fn.json_encode(variables),
       },
     })
   end
@@ -27,6 +29,7 @@ function M.query(query)
     return curl.request(graphql_url(), {
       body = {
         query = query,
+        variables = variables and vim.fn.json_encode(variables),
       },
     })
   end
@@ -35,13 +38,48 @@ function M.query(query)
 end
 
 function M.current_user_duo_status()
-  return M.query([[
+  return M.execute([[
     query currentUserDuoStatus {
       currentUser {
         duoCodeSuggestionsAvailable
       }
     }
   ]])
+end
+
+function M.project_settings(project)
+  return M.execute(
+    [[
+    query GetProject($project: ID!) {
+      project(fullPath: $project) {
+        duoFeaturesEnabled
+      }
+    }
+  ]],
+    { project = project }
+  )
+end
+
+function M.update_project_settings_duo_features_enabled(project, enabled)
+  vim.validate({
+    project = { project, 'string' },
+    enabled = { enabled, 'boolean' },
+  })
+
+  local variables = {
+    fullPath = project,
+    enabled = enabled,
+  }
+  return M.execute(
+    [[
+    mutation UpdateProjectSettings_duoFeaturesEnabled($fullPath: ID!, $enabled: Boolean!) {
+      projectSettingsUpdate(input: { fullPath: $fullPath, duoFeaturesEnabled: $enabled }) {
+        errors
+      }
+    }
+  ]],
+    variables
+  )
 end
 
 return M
